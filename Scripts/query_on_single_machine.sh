6070c06dd0842cf86d4fd27c587aa03d250ccb39
Melo Yang <meloyang05@gmail.com>
2023-05-11 16:35:05 +0800

    add .gitignore and update scripts

--- Scripts/query_on_single_machine.sh
+++ Scripts/query_on_single_machine.sh
@@ -7,6 +7,6 @@ docker run -it --mount type=bind,source=$PWD,target=/opt/resources\
                 -e CATALOG_PATH=/opt/resources/Catalogs/catalog\
                 -e SCHEMA_PATH=/opt/resources/Schemas/ldbc_schema.json\
                 -e DATA_PATH=/opt/resources/Graphs/scale_${scale}/bin_p1/partition_0\
-                -e PATTERN_PATH=/opt/resources/Patterns/p${id}.csv\
+                -e PATTERN_PATH=/opt/resources/Benchmark/gaia-benchmark-0.0.1-SNAPSHOT/Patterns/p${id}.csv\
                 glogs-executor:v1.0\
                 /opt/GraphScope/interactive_engine/executor/ir/target/release/pattern_match  -w $worker_num -s hybrid
