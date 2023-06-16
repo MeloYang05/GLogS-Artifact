00053012f7f6f075ead4acd8e0c30f69f6f94a79
Melo Yang <meloyang05@gmail.com>
2023-06-16 17:22:16 +0800

    add lsqb queries

--- Scripts/query_on_single_machine.sh
+++ Scripts/query_on_single_machine.sh
@@ -2,10 +2,16 @@ scale=$1
 worker_num=$2
 id=$3
 glogue=$4
+strategy=$5
 
 if [[ -z "$glogue" ]]
 then
     glogue="glogue"
+fi
+
+if [[ -z "$strategy" ]]
+then
+    strategy="hybrid"
 fi
 
 
@@ -15,4 +21,4 @@ docker run -it --mount type=bind,source=$PWD,target=/opt/resources\
                 -e DATA_PATH=/opt/resources/Graphs/scale_${scale}/bin_p1/partition_0\
                 -e PATTERN_PATH=/opt/resources/Benchmark/gaia-benchmark-0.0.1-SNAPSHOT/Patterns/p${id}.csv\
                 glogs-executor:v1.0\
-                /opt/GraphScope/interactive_engine/executor/ir/target/release/pattern_match  -w $worker_num -s hybrid
+                /opt/GraphScope/interactive_engine/executor/ir/target/release/pattern_match  -w $worker_num -s $strategy
